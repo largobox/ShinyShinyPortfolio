0c6d9a1cf895340e022f9c1e1025e8233926bfbe
danilov_sergey <sergey.event@gmail.com>
2016-11-09 11:34:52 +0300

    work section complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,7 @@
 $(function() {
 	smoothScroll(300);
+	workBelt();
+	workLoad();
 });
 
 function smoothScroll (duration) {
@@ -14,4 +16,36 @@ function smoothScroll (duration) {
 	        }, duration);
 	    }
 	});
+}
+
+function workBelt() {
+
+	$('.work_el').click(function(){
+		$('.work_belt').css('left', '-100%');
+		$('.work_desc_cnt').show();
+	});
+
+	$('.work_return').click(function(){
+		$('.work_belt').css('left', '0%');
+		$('.work_desc_cnt').hide(500);
+	});	
+
+}
+
+function workLoad() {
+
+	$.ajaxSetup({ cache: true });
+
+	$('.work_el').click(function(){
+
+		var newTitle = $(this).find('strong').text(),
+				newFolder = $(this).data('folder'),
+				spinner = '<div class="loader">Loading...</div>',
+				newHtml = '/work/'+ newFolder +'.html';
+
+		$('.project_load').html(spinner).load(newHtml);
+		$('.porject_title').text(newTitle);
+
+	});
+
 }
